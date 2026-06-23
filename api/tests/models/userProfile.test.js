0549b49c0b78c6ff7509e05d8ff2e29834e9fb35
jjcrl <j412cff@icloud.com>
2026-06-23 12:14:36 +0100

    testing of model

--- api/tests/models/userProfile.test.js
+++ api/tests/models/userProfile.test.js
@@ -8,6 +8,21 @@ describe("UserProfile model", () => {
   beforeEach(async () => {
     await UserProfile.deleteMany({})
   })
-
-  
+  test('requires a authUserId', async () => {
+    const profile = new UserProfile({})
+    await expect(profile.save()).rejects.toThrow()
+  });
+  test('favouriteArtists defaults to an empty array', async () => {
+    const profile = new UserProfile({ authUserId: "abc" })
+    await profile.save()
+    expect(profile.favouriteArtists).toEqual([])
+  })
+  test('home location field default to manchester', async () => {
+    const profile = new UserProfile({ authUserId: 'def' })
+    await profile.save()
+    // console.log(profile.homeLocation)
+     expect(profile.homeLocation.city).toBe("Manchester")
+      expect(profile.homeLocation.lat).toBe(53.483959)
+      expect(profile.homeLocation.long).toBe(-2.244644)
+  });
 })
