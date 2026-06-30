c9b24bebecbf7288305a9f2a0ed37346a72d373d
jjcrl <j412cff@icloud.com>
2026-06-30 10:08:45 +0100

    map page and back button

--- frontend/src/pages/Map/MapPage.jsx
+++ frontend/src/pages/Map/MapPage.jsx
@@ -1,12 +1,16 @@
 import Map from '../../components/Map'
 import { useState, useEffect, useMemo } from "react";
 import { getEvents } from '../../services/events';
+import NavBar from "../../components/NavBar"
+import Footer from "../../components/Footer"
+import { useNavigate } from 'react-router-dom';
 
 
 export function MapPage(props) {
     const [events, setEvents] = useState([]);
     const [loading, setLoading] = useState(true)
 
+    const navigate = useNavigate()
 
     useEffect(() => {
         getEvents({ city: "Manchester" })
@@ -19,8 +23,12 @@ export function MapPage(props) {
     return (
         <>
             <div className='map-page'>
+                <div className='go-back' onClick={(() => (navigate("/feed")))}>
+                    <button>back</button>
+                </div>
                 <Map events={events} height={"100%"} width={"100%"} zoom={12} />
             </div>
+            <Footer />
         </>
     )
 }
